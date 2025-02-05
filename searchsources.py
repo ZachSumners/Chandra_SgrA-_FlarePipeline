87bbb51e0e935594ea902055fb4f4bfe49b32924
ZachSumners <114012865+ZachSumners@users.noreply.github.com>
2025-02-05 13:47:46 -0500

    Update searchsources.py
    Cleaned and added documentation.

--- searchsources.py
+++ searchsources.py
@@ -1,24 +1,19 @@
 import subprocess
-import os
 import numpy as np
-import matplotlib.pyplot as plt
 from astropy.io import fits
-from matplotlib.patches import Ellipse
-from astropy.visualization import simple_norm
-#from pycrates import read_file
-from astropy.time import Time
-from datetime import datetime, timedelta
-import matplotlib.dates as mdates
-from astropy.table import Table
-import pandas as pd
 
 def find_sources(observationID, repro_wd, fileName):
+	'''This function looks at the event file and finds all sources in a specified box. It returns a list of ellipses fit to these sources.
+ 	This is a standard method used in the Guide to Analyzing Flares.
+  	'''
+
+	#Open event file, and do a PSF correction first.
 	subprocess.call('punlearn dmcopy', shell=True, cwd=repro_wd)
 	subprocess.call(f'dmcopy infile="acisf{observationID}_{fileName}_evt2.fits[EVENTS][bin x=3992.71:4174.71:1,y=3991.24:4173.24:1][energy=2000:8000]" outfile="{observationID}_{fileName}_2-8keV_cropped.fits" clobber=yes', shell=True, cwd=repro_wd)
 	subprocess.call('punlearn mkpsfmap', shell=True, cwd=repro_wd)
 	subprocess.call(f'mkpsfmap infile="{observationID}_{fileName}_2-8keV_cropped.fits" outfile="{observationID}_repro_2-8keV_psfmap.fits" energy=3.8 ecf=0.393 clobber=yes', shell=True, cwd=repro_wd)
 
-	#Search for sources in image.
+	#Search for sources in image and save detections to text file.
 	subprocess.call('punlearn wavdetect', shell=True, cwd=repro_wd)
 	subprocess.call(f'pset wavdetect infile="{observationID}_{fileName}_2-8keV_cropped.fits"', shell=True, cwd=repro_wd)
 	subprocess.call(f'pset wavdetect psffile="{observationID}_repro_2-8keV_psfmap.fits"', shell=True, cwd=repro_wd)
